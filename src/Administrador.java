b984fab35764cd256402747c3a1efd46ecbb5acd
camila546 <csanchezmo@ucenfotec.ac.cr>
2026-06-13 17:29:37 -0600

    Add Administrador class

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,3 +1,46 @@
-/* Clase de Camila (borrar este comment una vez completado el codigo) */
+
 public class Administrador {
+    private String correoElectronico;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    //constructor
+    public Administrador(String correoElectronico, String nombreUsuario, String contrasenia) {
+        this.correoElectronico = correoElectronico;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+    //getter
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+    //setter
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Administrador{" +
+                "correoElectronico='" + correoElectronico + '\'' +
+                ", nombreUsuario='" + nombreUsuario + '\'' +
+                ", contrasenia='" + contrasenia + '\'' +
+                '}';
+    }
 }
