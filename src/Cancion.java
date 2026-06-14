3c179a2360382072f38a4f6c6d1a4b08a7038eff
Alex Espinoza <alexandraeb20@gmail.com>
2026-06-13 22:55:36 -0600

    Nuevas variables y metodos agregados, mejora del codigo en general

--- src/Cancion.java
+++ src/Cancion.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
 public class Cancion {
+    private static final String CARATULA_PREDETERMINADA = "img/Record-icon.png";
+
     private String nombre;
     private String genero;
     private String artista;
@@ -11,20 +13,34 @@ public class Cancion {
     private double calificacion;
     private double precio;
 
+    private int cantidadCompras;
+    private int cantidadInclusionesEnListas;
+    private int cantidadCalificaciones;
+    private double sumaCalificaciones;
+
     /* Constructor */
-    public Cancion(String nombre, String genero, String artista, String compositor, LocalDate fechaLanzamiento, String album, String rutaCaratula, double calificacion, double precio) {
+    public Cancion(String nombre, String genero, String artista, String compositor,
+                   LocalDate fechaLanzamiento, String album, String rutaCaratula,
+                   double calificacion, double precio) {
         this.nombre = nombre;
         this.genero = genero;
         this.artista = artista;
         this.compositor = compositor;
         this.fechaLanzamiento = fechaLanzamiento;
         this.album = album;
-        this.rutaCaratula = rutaCaratula;
-        this.calificacion = calificacion;
-        this.precio = precio;
+
+        setRutaCaratula(rutaCaratula);
+        setCalificacion(calificacion);
+        setPrecio(precio);
+
+        this.cantidadCompras = 0;
+        this.cantidadInclusionesEnListas = 0;
+        this.cantidadCalificaciones = 0;
+        this.sumaCalificaciones = 0.0;
     }
 
-    /* Getter & Setters */
+    /* Getters & Setters */
+
     public String getNombre() {
         return nombre;
     }
@@ -78,7 +94,11 @@ public class Cancion {
     }
 
     public void setRutaCaratula(String rutaCaratula) {
-        this.rutaCaratula = rutaCaratula;
+        if (rutaCaratula == null || rutaCaratula.isBlank()) {
+            this.rutaCaratula = CARATULA_PREDETERMINADA;
+        } else {
+            this.rutaCaratula = rutaCaratula;
+        }
     }
 
     public double getCalificacion() {
@@ -86,7 +106,9 @@ public class Cancion {
     }
 
     public void setCalificacion(double calificacion) {
-        this.calificacion = calificacion;
+        if (calificacion >= 0.0 && calificacion <= 5.0) {
+            this.calificacion = Math.round(calificacion * 10.0) / 10.0;
+        }
     }
 
     public double getPrecio() {
@@ -94,6 +116,86 @@ public class Cancion {
     }
 
     public void setPrecio(double precio) {
-        this.precio = precio;
+        if (precio >= 0.0) {
+            this.precio = Math.round(precio * 100.0) / 100.0;
+        }
+    }
+
+    public int getCantidadCompras() {
+        return cantidadCompras;
+    }
+
+    public void setCantidadCompras(int cantidadCompras) {
+        if (cantidadCompras >= 0) {
+            this.cantidadCompras = cantidadCompras;
+        }
+    }
+
+    public int getCantidadInclusionesEnListas() {
+        return cantidadInclusionesEnListas;
+    }
+
+    public void setCantidadInclusionesEnListas(int cantidadInclusionesEnListas) {
+        if (cantidadInclusionesEnListas >= 0) {
+            this.cantidadInclusionesEnListas = cantidadInclusionesEnListas;
+        }
+    }
+
+    public int getCantidadCalificaciones() {
+        return cantidadCalificaciones;
+    }
+
+    public void setCantidadCalificaciones(int cantidadCalificaciones) {
+        if (cantidadCalificaciones >= 0) {
+            this.cantidadCalificaciones = cantidadCalificaciones;
+        }
+    }
+
+    public double getSumaCalificaciones() {
+        return sumaCalificaciones;
+    }
+
+    public void setSumaCalificaciones(double sumaCalificaciones) {
+        if (sumaCalificaciones >= 0.0) {
+            this.sumaCalificaciones = sumaCalificaciones;
+        }
+    }
+
+    /* Metodos */
+
+    public void registrarCompra() {
+        cantidadCompras++;
+    }
+
+    public void registrarInclusionEnLista() {
+        cantidadInclusionesEnListas++;
+    }
+
+    public boolean agregarCalificacion(double nuevaCalificacion) {
+        if (nuevaCalificacion >= 0.0 && nuevaCalificacion <= 5.0) {
+            sumaCalificaciones += nuevaCalificacion;
+            cantidadCalificaciones++;
+            calificacion = Math.round((sumaCalificaciones / cantidadCalificaciones) * 10.0) / 10.0;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" +
+                "nombre='" + nombre + '\'' +
+                ", genero='" + genero + '\'' +
+                ", artista='" + artista + '\'' +
+                ", compositor='" + compositor + '\'' +
+                ", fechaLanzamiento=" + fechaLanzamiento +
+                ", album='" + album + '\'' +
+                ", rutaCaratula='" + rutaCaratula + '\'' +
+                ", calificacion=" + calificacion +
+                ", precio=" + precio +
+                ", cantidadCompras=" + cantidadCompras +
+                ", cantidadInclusionesEnListas=" + cantidadInclusionesEnListas +
+                ", cantidadCalificaciones=" + cantidadCalificaciones +
+                '}';
     }
 }
