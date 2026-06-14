e8aa7ded511925f9f60b8546b4ad05a6b4ea5a9f
Alex Espinoza <alexandraeb20@gmail.com>
2026-06-13 18:03:34 -0600

    Actualizacion inicial de la clase Cancion

--- src/Cancion.java
+++ src/Cancion.java
@@ -1,3 +1,99 @@
-/* Clase de Alex (borrar este comment una vez completado el codigo) */
+import java.time.LocalDate;
+
 public class Cancion {
+    private String nombre;
+    private String genero;
+    private String artista;
+    private String compositor;
+    private LocalDate fechaLanzamiento;
+    private String album;
+    private String rutaCaratula;
+    private double calificacion;
+    private double precio;
+
+    /* Constructor */
+    public Cancion(String nombre, String genero, String artista, String compositor, LocalDate fechaLanzamiento, String album, String rutaCaratula, double calificacion, double precio) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.artista = artista;
+        this.compositor = compositor;
+        this.fechaLanzamiento = fechaLanzamiento;
+        this.album = album;
+        this.rutaCaratula = rutaCaratula;
+        this.calificacion = calificacion;
+        this.precio = precio;
+    }
+
+    /* Getter & Setters */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(String compositor) {
+        this.compositor = compositor;
+    }
+
+    public LocalDate getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(LocalDate fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getRutaCaratula() {
+        return rutaCaratula;
+    }
+
+    public void setRutaCaratula(String rutaCaratula) {
+        this.rutaCaratula = rutaCaratula;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
