7f3683b92d3316962186d00797bbc2b418452b8c
camila546 <csanchezmo@ucenfotec.ac.cr>
2026-06-13 22:05:43 -0600

    definicion de metodos

--- src/ListaReproduccion.java
+++ src/ListaReproduccion.java
@@ -1,18 +1,20 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ListaReproduccion {
     private String nombre;
     private LocalDate fechaCreacion;
     private double calificacion;
-    private Cancion[] canciones;
+    private ArrayList<Cancion> canciones;
 
     //Constructor
 
-    public ListaReproduccion(String nombre, LocalDate fechaCreacion, double calificacion) {
+    public ListaReproduccion(String nombre, LocalDate fechaCreacion) {
         this.nombre = nombre;
         this.fechaCreacion = fechaCreacion;
-        this.calificacion = calificacion;
+        this.calificacion = 0.0;
+        this.canciones = new ArrayList<>();
     }
     //getter y setter
     public String getNombre() {
@@ -27,16 +29,18 @@ public class ListaReproduccion {
         return calificacion;
     }
 
-    public Cancion[] getCanciones() {
-        return canciones;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     public void setFechaCreacion(LocalDate fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
+    }
+    public ArrayList<Cancion> getCanciones() {
+        return canciones;
+    }
+    public void setCanciones(ArrayList<Cancion> canciones) {
+        this.canciones = canciones;
     }
 
     public void setCalificacion(double calificacion) {
@@ -49,7 +53,20 @@ public class ListaReproduccion {
                 "nombre='" + nombre + '\'' +
                 ", fechaCreacion=" + fechaCreacion +
                 ", calificacion=" + calificacion +
-                ", canciones=" + Arrays.toString(canciones) +
+                ", cantidadCanciones=" + canciones.size() +
                 '}';
     }
+    //metodos
+    public void agregarCancion(Cancion cancion) {
+        System.out.println("Agregando " + cancion.getNombre() + " a la lista " + nombre);
+    }
+
+    public void eliminarCancion(Cancion cancion) {
+        System.out.println("Eliminando " + cancion.getNombre() + " de la lista " + nombre);
+    }
+    public double calcularCalificacionPromedio() {
+        System.out.println("Recorriendo objetos Cancion en '" + this.nombre + "' para dar promedio de calificacion");
+        return 4.5; // Retorna un valor de prueba que esté en el rango de 0.0 a 5.0
+    }
+
 }
