158a4acb29de2f7bb2fa012cbcdd272f9d3687bd
amontiels-ucenfotec <amontiels@ucenfotec.ac.cr>
2026-07-09 21:11:28 -0600

    SE AGREGA MAIN Y SE ACTUALIZA FUNCIONALIDAD

--- src/excepciones/CredencialesInvalidasException.java
+++ src/excepciones/CredencialesInvalidasException.java
@@ -1,7 +1,5 @@
 package excepciones;
 
-/* Excepción lanzada cuando las credenciales ingresadas por un usuario
-   no coinciden con las registradas en el sistema. */
 public class CredencialesInvalidasException extends Exception {
 
     public CredencialesInvalidasException(String mensaje) {
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,5 +5,10 @@ import logica.Aplicacion;
 public class Main {
     public static void main(String[] args) {
 
+        // Se crea la aplicación y se delega toda la interacción a la capa de interfaz.
+        Aplicacion aplicacion = new Aplicacion();
+        MenuConsola menu = new MenuConsola(aplicacion);
+        menu.iniciar();
+
     }
 }
