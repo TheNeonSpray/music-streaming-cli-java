90bb1f932ca14edbc22cb6ba07bc08dbd13ac945
camila546 <csanchezmo@ucenfotec.ac.cr>
2026-07-09 21:11:04 -0600

    Cambios de Cancion y se añadio la interfaz de repoducible

--- src/entidades/Reproducible.java
+++ src/entidades/Reproducible.java
@@ -1,4 +1,11 @@
 package entidades;
 
+/* Interfaz que define el comportamiento común de los elementos que pueden
+   reproducirse dentro de la aplicación (canciones y listas de reproducción).
+   La reproducción se realiza de forma simbólica: el método retorna el mensaje
+   correspondiente y la capa de interfaz se encarga de mostrarlo. */
 public interface Reproducible {
+
+    // Se retorna el mensaje simbólico de reproducción del elemento.
+    String reproducir();
 }
